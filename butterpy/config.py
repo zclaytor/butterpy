e5ae07015ca9e07c96e342e650ee892416534803
Miles Lucas <mdlucas@hawaii.edu>
2019-10-28 13:12:13 -1000

    put in folder

---
+++ butterpy/config.py
@@ -0,0 +1,10 @@
+root = '/home/zach/PhD/tess_sim/'
+tess_dir = root + 'ETE6/'
+tess_stars_dir = root + 'tess_stars.csv'
+sim_dir = root + 'lightcurves/'
+simulation_properties_dir = sim_dir + 'simulation_properties.csv'
+
+
+Nlc = 1000 # How many light curves are we making? 
+dur = 3650 # Duration in days
+cad = 30 # cadence in minutes
