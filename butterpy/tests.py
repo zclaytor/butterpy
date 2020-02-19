cda1237b3008230ebeb943a20f1c8b4cd0cf3cb0
zclaytor <zclaytor@hawaii.edu>
2020-02-19 10:01:40 -1000

    formatting to improve readability

--- butterpy/tests.py
+++ butterpy/tests.py
@@ -10,18 +10,40 @@ def sun_test():
     ani = star_test()
     return ani
 
-def star_test(activity_rate=1, cycle_length=11, cycle_overlap=2, inclination=1,
-        spot_min=7, spot_max=35, period=24.5, shear=0.2, decay_time=5, 
-        butterfly=True, t1=0, t2=3650, tstep=1):
-        
+def star_test(
+    activity_rate=1, 
+    cycle_length=11, 
+    cycle_overlap=2, 
+    inclination=1,
+    spot_min=7, 
+    spot_max=35, 
+    period=24.5, 
+    shear=0.2, 
+    decay_time=5, 
+    butterfly=True, 
+    t1=0, 
+    t2=3650, 
+    tstep=1
+):
     print('Generating spot evolution for the star...')
-    star = regions(butterfly=butterfly, activity_rate=activity_rate, 
-        cycle_length=cycle_length, cycle_overlap=cycle_overlap, 
+    star = regions(
+        butterfly=butterfly, 
+        activity_rate=activity_rate, 
+        cycle_length=cycle_length, 
+        cycle_overlap=cycle_overlap, 
         decay_time=(period*decay_time),
-        max_ave_lat=spot_max, min_ave_lat=spot_min,
-        alpha_med=(activity_rate*FLUX_SCALE))
-    spots = Spots(star, alpha_med=(activity_rate*FLUX_SCALE), period=period,
-        incl=inclination, decay_timescale=decay_time, diffrot_shear=shear)
+        max_ave_lat=spot_max, 
+        min_ave_lat=spot_min,
+        alpha_med=(activity_rate*FLUX_SCALE)
+    )
+    spots = Spots(
+        star, 
+        alpha_med=(activity_rate*FLUX_SCALE), 
+        period=period,
+        incl=inclination, 
+        decay_timescale=decay_time, 
+        diffrot_shear=shear
+    )
     
     print('Generating light curve...')
     time = np.arange(t1, t2, tstep)
