e41188cfab5abac5fe1e120e99ba8d1a2bb05aba
zclaytor <zclaytor@hawaii.edu>
2021-04-20 13:59:31 -1000

    add version.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     readme = fh.read()
 
+with open("butterpy/version.py", "r") as fv:
+    exec(fv.read())
+
 setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     name="butterpy",
-    version="0.1.0",
+    version=__version__,
     python_requires="==3.*,>=3.6.0",
     author="Zachary Claytor <zclaytor@hawaii.edu> and Miles Lucas <mdlucas@hawaii.edu>",
     packages=find_packages(),
