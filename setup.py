2cca324382ba07a773e797ad3decf85b1ae22a33
zclaytor <zclaytor@hawaii.edu>
2021-04-20 15:02:38 -1000

    fix OS classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux/MacOS",
+        "Operating System :: Unix",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],
