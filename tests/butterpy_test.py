8082be1e3430f66eb6e99fac19ba39a0ce058c98
Zachary Claytor <zclaytor@ufl.edu>
2022-11-22 15:56:08 -0500

    add tests

---
+++ tests/butterpy_test.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+import numpy as np
+from astropy.table import Table
+from butterpy.core_original import regions
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.fixture
+def default_surface():
+    np.random.seed(88)
+    return regions(
+        activityrate=1, minlat=5, maxlat=35, 
+        cyclelength=3, cycleoverlap=1, tsim=3600)
+    
+def test_regions(default_surface):
+    surface = default_surface
+    assert isinstance(surface, Table)
+
+def test_output(default_surface):
+    surface = default_surface
+    expected = Table.read(os.path.join(cwd, "data/default_surface.fits"))
+    assert surface == pytest.approx(expected)
