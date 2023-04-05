30a32019fc63fbb7f8e230eba489ffff02981e85
zclaytor <zclaytor@hawaii.edu>
2023-04-04 22:04:53 -0400

    commit solar normalization utilities

---
+++ butterpy/utils/normalization.py
@@ -0,0 +1,92 @@
+"""
+Utilities for Solar normalization.
+
+Contains
+--------
+solar_regions (function):
+    Runs the `regions` function with Solar inputs.
+
+fit_spot_counts (function):
+    Computes spot counts and fits a squared sine function
+    to the monthly spot count.
+
+NOTE
+----
+According to Hathaway (2015), LRSP 12: 4, Figure 2, the peak monthly
+    sunspot number varies from cycle to cycle, with peaks as low as 
+    50 spots/month (1818, Cycle 6) and as high as 200 (1960, Cycle 19).
+    The Solar normalization should return an amplitude somewhere in this range.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from butterpy.core_original import regions
+from plots import monthly_spot_number
+
+
+def solar_regions():
+    """
+    Runs the `regions` function with Solar inputs:
+
+    activityrate = 1,
+    minlat = 7,
+    maxlat = 35,
+    cyclelength = 11,
+    cycleoverlap = 1,
+    ndays = 100 years
+    """
+    return regions(activityrate=1, minlat=7, maxlat=35, 
+        cyclelength=11, cycleoverlap=1, ndays=365*100)
+
+
+def fit_spot_counts(spots, make_plot=True):
+    """
+    Computes spot counts and fits the function
+
+        A sin^2(pi*t/P + phi),
+    
+    where A is the amplitude, t is time, P is the cycle period,
+    and phi is a temporal shift. The form of this function is chosen
+    to resemble the form of the emergence rate, defined in `regions`.
+
+    Parameters
+    ----------
+    spots (astropy Table):
+        The output of `regions` containing the table of star spots.
+
+    make_plot (bool, optional, default=True):
+        Whether to make and display the plot.
+
+    Returns
+    -------
+    A (float):
+        The spot count amplitude with units of spots/month
+    """
+    time, nspots = monthly_spot_number(spots, make_plot=False)
+
+    # Fit Asin^2(pi*t/P + phi), based on emergence rate definition.
+    from scipy.optimize import curve_fit
+
+    def f(t, A, P, phi):
+        return A*np.sin(np.pi*t/P + phi)**2
+    
+    T = np.array(time)
+    N = np.array(nspots)
+    popt, _ = curve_fit(f, T, N, p0=(25, 11, 0))
+    A = popt[0]
+
+    if make_plot:
+        plt.figure()
+        plt.plot(T, N, "k", label="Smoothed Spot Count")
+        plt.plot(T[::10], f(T[::10], *popt), "bo", ms=8, label=f"Model with A={A:.2f}")
+        plt.xlabel("Time (years)")
+        plt.ylabel("Monthly Sunspot Number")
+        plt.legend()
+        plt.show()
+    return A
+
+if __name__ == "__main__":
+    np.random.seed(88)
+    sun = solar_regions()
+    A = fit_spot_counts(sun, make_plot=False)
+    print(f"amplitude: {A} spots/month")
--- butterpy/utils/plots.py
+++ butterpy/utils/plots.py
@@ -1,17 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from butterpy.core_original import regions
 
-if __name__ == "__main__":
-    np.random.seed(88)
-    r = regions(activityrate=1, minlat=30, maxlat=35, 
-        cyclelength=3, cycleoverlap=1, ndays=3600)
-    thcen = 90*(1 - (r["thpos"] + r["thneg"])/np.pi) #type:ignore
+
+def butterfly(r):
+    """Plot the butterfly diagram of a regions table.
+    """
+    thcen = 90*(1 - (r["thpos"] + r["thneg"])/np.pi)
     ndays = r["nday"]
 
     plt.figure()
-    plt.scatter(ndays/365, thcen, c="k", alpha=0.5) #type:ignore
+    plt.scatter(ndays/365, thcen, c="k", alpha=0.5)
     plt.xlabel("Time (years)")
     plt.ylabel("Latitude (deg)")
     plt.title(f"{len(r)} spots")
     plt.show()
+
+
+def monthly_spot_number(spots, make_plot=True):
+    """
+    Compute and plot the monthly spot number.
+
+    Following Hathaway (2015), LRSP 12: 4, we compute the number of spots
+    emerging in a particular month, then smooth the time series with a
+    sliding 13-month window, centered on each month. This corresponds to 
+    the "International Sunspot Number" (see section 3.1).
+
+    Parameters
+    ----------
+    spots (astropy Table):
+        The output of `regions` containing the table of star spots.
+
+    make_plot (bool, optional, default=True):
+        Whether to make and display the plot.
+
+    Returns
+    -------
+    T (list):
+        The list of times corresponding to each spot count.
+
+    N (list):
+        The list of spot counts.
+    """
+    days = spots["nday"]
+    bin_days = 30 
+
+    # first bin spot counts by month
+    d = bin_days/2
+    time = []
+    nspots = []
+    while d < days.max():
+        time.append(d)
+        nspots.append(sum((d-bin_days/2 < days) & (days <= d+bin_days/2)))
+        d += bin_days
+    
+    # smooth monthly spot counts
+    time = np.array(time)
+    nspots = np.array(nspots)
+    bin_days = 30*13
+    d = bin_days/2 
+    T = []
+    N = []
+    while d < days.max():
+        T.append(d/365)
+        N.append(np.mean(nspots[((d-bin_days/2) < time) & (time <= (d+bin_days/2))]))
+        d += bin_days/13
+
+    if make_plot:
+        plt.figure()
+        plt.plot(time/365, nspots, "ko", ms=3, label="Monthly Spot Count")
+        plt.plot(T, N, "r", label="13-Month Smoothing")
+        plt.xlabel("Time (years)")
+        plt.ylabel("Monthly Sunspot Number")
+        plt.legend()
+        plt.show()
+
+    return T, N
