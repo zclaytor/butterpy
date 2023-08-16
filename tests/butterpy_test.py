e7e8a6dc883d16954af6aaf5239fb13f32da5eed
zclaytor <zclaytor@hawaii.edu>
2023-08-16 09:50:57 -0400

    add spots test; clean up regions test

--- tests/butterpy_test.py
+++ tests/butterpy_test.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import numpy as np
 from astropy.table import Table
-from butterpy.core import regions
+from butterpy.core import regions, spots
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -16,16 +16,25 @@ def default_surface():
         cyclelength=3, cycleoverlap=1, ndays=3600)
     
 @pytest.fixture
-def load_test_data():
+def load_test_surface():
     return Table.read(os.path.join(cwd, "data/default_surface.fits"))
+
+@pytest.fixture
+def load_test_flux():
+    return np.load(os.path.join(cwd, "data/default_spots.npy"))
 
 def test_regions(default_surface):
     surface = default_surface
     assert isinstance(surface, Table), "Result of `regions` call is not an astropy Table."
 
-def test_output(default_surface, load_test_data):
+def test_regions_output(default_surface, load_test_surface):
     surface = default_surface
-    expected = load_test_data
-    assert len(surface) == len(expected), "Length of `regions` output does not match test data."
-    assert surface["thpos"][0] == expected["thpos"][0], "First `thpos` values do not match."
-    assert surface["nday"][-1] == expected["nday"][-1], "Final `nday` values do not match."
+    expected = load_test_surface
+    assert surface == pytest.approx(expected), "Calculated surface does not match expectation."
+
+def test_flux(default_surface, load_test_flux):
+    s = spots(default_surface)
+    time = np.arange(0, 3600, 0.1)
+    f_calc = s.calc(time)
+    f_expected = load_test_flux
+    assert f_calc == pytest.approx(f_expected), "Calculated flux does not match expectation."
