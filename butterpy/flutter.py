693260c9d637cd399e624ea0e59c5ac7712bd517
zclaytor <zclaytor@hawaii.edu>
2023-11-07 22:02:49 -0500

    add basic Flutter framework, distributions

--- butterpy/flutter.py
+++ butterpy/flutter.py
@@ -1,77 +1,69 @@
 import numpy as np
 import pandas as pd
 
+from .distributions import Uniform, LogUniform, SineSquared, Boolean, Composite, Fixed
 
 class Flutter(object):
     """Sets the environment for a group of Butterpy Surface simulations.
     """
-    def __init__(self):
+    def __init__(
+        self,
+        duration=365,
+        cadence=1/48,
+        n_sims=10,
+        activity_level=LogUniform(0.1, 10),
+        butterfly=Boolean(p=0.8),
+        cycle_period=LogUniform(1, 40),
+        cycle_overlap=None,
+        min_lat=Uniform(0, 40),
+        max_lat=None,
+        inclination=SineSquared(0, np.pi/2),
+        period=Uniform(0.1, 180),
+        shear=None,
+        tau_evol=LogUniform(1, 10)):
         """DOCSTRING
         """
-        self.duration = None
-        self.cadence = None
-        self.n_sims = None
+        self.duration = duration
+        self.cadence = cadence
+        self.n_sims = n_sims
         
-        self.activity_level = None
-        self.butterfly = None
-        self.cycle_period = None
-        self.cycle_overlap = None
-        self.max_lat = None
-        self.min_lat = None
+        self.activity_level = activity_level
+        self.butterfly = butterfly
+        self.cycle_period = cycle_period
+        if cycle_overlap is None:
+            cycle_overlap = LogUniform(0.1, self.cycle_period)
+        self.cycle_overlap = cycle_overlap
+        self.min_lat = min_lat
+        if max_lat is None:
+            max_lat = Uniform(self.min_lat+5, 85)
+        self.max_lat = max_lat
 
-        self.incl = None 
-        self.period = None
-        self.shear = None
-        self.tau_evol = None
+        self.inclination = inclination
+        self.period = period
+        if shear is None:
+            shear = Composite(
+                [LogUniform(-1, 0), LogUniform(0, 1), Fixed(0)],
+                weights=[0.25, 0.5, 0.25])
+        self.shear = shear
+        self.tau_evol = tau_evol
 
-        # inclination ~ uniform in sin^2(i)
-        incl = np.arcsin(np.sqrt(np.random.uniform(0, 1, Nlc)))
-        # activity rate ~ uniform
-        # ar = 10**np.random.uniform(low=-2, high=1, size=Nlc)
-        ar = np.random.uniform(low=0.1, high=10, size=Nlc)
-        # cycle length ~ log uniform
-        clen = 10 ** np.random.uniform(low=0, high=1.6, size=Nlc)
-        # cycle overlap ~ log uniform, but max out at cycle length.
-        max_cover = np.maximum(np.log10(clen), 0.5)
-        # cover = 10**np.random.uniform(low=-1, high=0.5, size=Nlc)
-        cover = 10 ** np.random.uniform(low=-1, high=max_cover)
-        # minimum and maximum spot latitudes
-        theta_low = np.random.uniform(low=0, high=40, size=Nlc)
-        theta_high = np.random.uniform(low=theta_low + 5, high=80)
-        # period ~ uniform
-        # period = 10.0**np.random.uniform(low=-1, high=2, size=Nlc)
-        period = np.random.uniform(low=0.1, high=365, size=Nlc)
-        # spot decay timescale ~ log uniform
-        tau_evol = 10.0 ** np.random.uniform(low=0, high=1, size=Nlc)
-        # if butterfly==True, spot emergence latitude has cycle phase dependence
-        butterfly = np.random.choice([True, False], size=Nlc, p=[0.8, 0.2])
-        # differential rotation shear ~ log uniform, and allow for negative values
-        diffrot_shear = np.zeros(Nlc)
-        n_pos = int(Nlc * 0.5)
-        n_neg = int(Nlc * 0.25)
-        diffrot_shear[:n_pos] = 10 ** np.random.uniform(-1, 0, size=n_pos)
-        diffrot_shear[n_pos : n_pos + n_neg] = -10 ** np.random.uniform(-1, 0, size=n_neg)
-        np.random.shuffle(diffrot_shear)
+        self.DataFrame = None
 
-        omega = 2 * np.pi / period # rad / day
+    def sample(self):
+        self.DataFrame = pd.DataFrame([])
+        
+        self.DataFrame["activity_level"] = self.activity_level.sample(self.n_sims)
+        self.DataFrame["cycle_period"] = self.cycle_period.sample(self.n_sims)
+        self.DataFrame["cycle_overlap"] = self.cycle_overlap.sample(self.n_sims)
+        self.DataFrame["inclination"] = self.inclination.sample(self.n_sims)
+        self.DataFrame["min_lat"] = self.min_lat.sample(self.n_sims)
+        self.DataFrame["max_lat"] = self.max_lat.sample(self.n_sims)
+        self.DataFrame["period"] = self.period.sample(self.n_sims)
+        self.DataFrame["shear"] = self.shear.sample(self.n_sims)
+        self.DataFrame["tau_evol"] = self.tau_evol.sample(self.n_sims)
+        self.DataFrame["butterfly"] = self.butterfly.sample(self.n_sims)
 
-
-        # Stitch this all together and write the simulation properties to file
-        sims = {}
-        sims["Activity Rate"] = ar
-        sims["Cycle Length"] = clen
-        sims["Cycle Overlap"] = cover
-        sims["Inclination"] = incl
-        sims["Spot Min"] = theta_low
-        sims["Spot Max"] = theta_high
-        sims["Period"] = period
-        sims["Omega"] = omega
-        sims["Shear"] = diffrot_shear
-        sims["Decay Time"] = tau_evol
-        sims["Butterfly"] = butterfly
-        sims = pd.DataFrame.from_dict(sims)
-        sims.to_csv(sim_dir + "simulation_properties.csv", float_format="%5.4f",
-            index_label="Simulation Number")
+        self.DataFrame.index.name = "simulation_number"
 
 
     def make_plots(self):
@@ -79,10 +71,10 @@ class Flutter(object):
         """
         pass
 
-    def to_csv(self):
+    def to_csv(self, path, **kw):
         """DOCSTRING
         """
-        pass
+        self.DataFrame.to_csv(path, **kw)        
 
     def __repr__(self):
         """Gonna need a repr to print out what distros are set.
